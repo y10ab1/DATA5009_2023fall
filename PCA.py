189a4fc37a447eaa767af18a36c7d095bb715bb5
y10ab1 <brian880825@gmail.com>
2023-09-23 17:10:47 +0800

    PCA

---
+++ PCA.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+class PCA:
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.components = None
+        self.mean = None
+
+    def fit(self, X):
+        # mean
+        self.mean = np.mean(X, axis=0)
+        X = X - self.mean
+
+        # covariance
+        cov = np.cov(X.T)
+
+        # eigenvalues, eigenvectors
+        eigenvalues, eigenvectors = np.linalg.eig(cov)
+
+        # sort eigenvalues and eigenvectors
+        eigenvectors = eigenvectors.T
+        idxs = np.argsort(eigenvalues)[::-1]
+        eigenvalues = eigenvalues[idxs]
+        eigenvectors = eigenvectors[idxs]
+
+        # store first n eigenvectors
+        self.components = eigenvectors[0:self.n_components]
+
+    def transform(self, X):
+        # project data
+        X = X - self.mean
+        return np.dot(X, self.components.T)
+
+    def fit_transform(self, X):
+        self.fit(X)
+        return self.transform(X)
+    
+if __name == '__main__':
+    import matplotlib.pyplot as plt
+    from sklearn import datasets
+    from sklearn.model_selection import train_test_split
+
+    data = datasets.load_iris()
+    X = data.data
+    y = data.target
+
+    # Project the data onto the 2 primary principal components
+    pca = PCA(2)
+    X_projected = pca.fit_transform(X)
+
+    print('Shape of X:', X.shape)
+    print('Shape of transformed X:', X_projected.shape)
+
+    x1 = X_projected[:, 0]
+    x2 = X_projected[:, 1]
+
+    plt.scatter(x1, x2,
+                c=y, edgecolor='none', alpha=0.8,
+                cmap=plt.cm.get_cmap('viridis', 3))
+
+    plt.xlabel('Principal Component 1')
+    plt.ylabel('Principal Component 2')
+    plt.colorbar()
+    plt.show()
