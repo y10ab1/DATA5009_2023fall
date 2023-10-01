d71003931ee3710c15a63f37f8f53177c92a3dd4
y10ab1 <brian880825@gmail.com>
2023-10-02 01:12:55 +0800

    2-a

--- hw1/2-a.py
+++ hw1/2-a.py
@@ -11,8 +11,10 @@ def var_covar_matrix(X):
     """
     # centering X along each row
     X_centered = np.apply_along_axis(centering, 1, X)
+    # print(X_centered.round(2))
+    
     # compute the variance-covariance matrix of X
-    var_covar_matrix = np.dot(X_centered.T, X_centered) / (X.shape[0] - 1)
+    var_covar_matrix = np.dot(X_centered, X_centered.T) / (X.shape[1] - 1)
     return var_covar_matrix
 
 if __name__ == '__main__':
@@ -22,4 +24,7 @@ if __name__ == '__main__':
     # compute the variance-covariance matrix of X_picked and print it
     var_covar_matrix = var_covar_matrix(X_picked)
     print(var_covar_matrix.round(2))
+
+    # validate the result using np.cov
+    print(f'var_covar_matrix is correct: {np.allclose(var_covar_matrix, np.cov(X_picked))}')
     
