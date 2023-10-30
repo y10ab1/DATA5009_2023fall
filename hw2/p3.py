97984353557c07ebc267d1072fcaf9e79293fd77
y10ab1 <brian880825@gmail.com>
2023-10-31 02:45:45 +0800

    update

--- hw2/p3.py
+++ hw2/p3.py
@@ -29,13 +29,49 @@ def pi_estimation_v2(N=10000, h=1, r=1):
         x_new, y_new = x + eps_x, y + eps_y
         if in_square(x_new, y_new):
             x, y = x_new, y_new
-            pi_hat += 1 if in_circle(x, y) else 0
+        pi_hat += 1 if in_circle(x, y) else 0
             
     return 4 * pi_hat / N
+
+def exp_on_n_and_h():
+    N = 10000
+    hs = [0.125, 0.25, 0.5, 1, 2, 4]
+    ns = [100, 1000, 5000, 10000]
+    
+    # Plotting the results in same figure
+    # visualize the results of different h and n
+    fig, ax = plt.subplots(1, 2)
+    for h in hs:
+        for n in ns:
+            ori_pi = pi_estimation(n, h)
+            improved_pi = pi_estimation_v2(n, h)
+            print(f"Estimate of pi (h={h}, n={n}): {ori_pi}")
+            print(f"Estimate of pi (h={h}, n={n}, improved): {improved_pi}")
+            ax[0].scatter(np.log(h), ori_pi, color="blue")
+            ax[0].scatter(np.log(h), improved_pi, color="red")
+            ax[1].scatter(np.log(n), ori_pi, color="blue")
+            ax[1].scatter(np.log(n), improved_pi, color="red")
+    ax[0].set_xlabel("h (log scale)")
+    ax[0].set_ylabel("pi")
+    ax[0].set_title("pi vs h")
+    ax[1].set_xlabel("n (log scale)")
+    ax[1].set_ylabel("pi")
+    ax[1].set_title("pi vs n")
+    ax[0].legend(["original", "improved"])
+    ax[1].legend(["original", "improved"])
+    ax[0].hlines(y=np.pi, xmin=0, xmax=10, color="black", linestyle="--")
+    ax[1].hlines(y=np.pi, xmin=0, xmax=10, color="black", linestyle="--")
+    plt.savefig("p3.png")
+    
+
+    
 
 def main():
     print(f"Estimate of pi (flawed): {pi_estimation()}")
     print(f"Estimate of pi (improved): {pi_estimation_v2()}")
+
+    exp_on_n_and_h()
+
 
 if __name__ == "__main__":
     main()
