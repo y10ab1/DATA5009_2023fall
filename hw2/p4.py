f4b0c5079f5179fbf4339b497b4c88eb7d81c8fe
y10ab1 <brian880825@gmail.com>
2023-10-31 18:29:29 +0800

    update

--- hw2/p4.py
+++ hw2/p4.py
@@ -1,7 +1,15 @@
 import pyforest
 
+def get_params():
+    theta = np.random.uniform(1, 111) # 1 <= theta <= 111
+    a1, a2 = np.random.gamma(10, 10, 2) # a1, a2 ~ Gamma(10, 10)
+    lambda1 = np.random.gamma(3, a1) # lambda1 ~ Gamma(3, a1)
+    lambda2 = np.random.gamma(3, a2) # lambda2 ~ Gamma(3, a2)
+    return theta, lambda1, lambda2
+    
 def main():
-    pass
+    theta, lambda1, lambda2 = get_params()
+    print("theta: ", theta, "lambda1: ", lambda1, "lambda2: ", lambda2)
 
 if __name__ == "__main__":
     main()
