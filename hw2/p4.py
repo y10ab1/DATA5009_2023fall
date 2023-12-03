66fb3c31c9a571b3696a905007aa37317b362f4b
y10ab1 <brian880825@gmail.com>
2023-12-03 12:54:55 +0800

    update

--- hw2/p4.py
+++ hw2/p4.py
@@ -80,6 +80,7 @@ def a():
     axes[2].set_title('Lambda2')
     plt.savefig('p4_a.png')
     print('Mean of theta: %f, lambda1: %f, lambda2: %f' % (np.mean(thetas), np.mean(lambdas1), np.mean(lambdas2)))
+    return thetas, lambdas1, lambdas2
 
 def b():
     data = get_synthetic_data()
@@ -109,7 +110,64 @@ def c():
     plt.savefig('p4_c.png')
     print('Mean of theta: %f, lambda1: %f, lambda2: %f' % (np.mean(thetas), np.mean(lambdas1), np.mean(lambdas2)))    
 
+
+
+def autocorrelation(samples, max_lag):
+    def compute_lag(samples, lag):
+        n = len(samples)
+        mean_sample = np.mean(samples)
+        if lag >= n:
+            return 0  # Autocorrelation for full length is 0
+        numerator = np.sum((samples[:-lag] - mean_sample) * (samples[lag:] - mean_sample))
+        denominator = np.sum((samples - mean_sample) ** 2)
+        return numerator / denominator
+
+    return [compute_lag(samples, lag) for lag in range(1, max_lag + 1)]
+
+
+    return [compute_lag(samples, lag) for lag in range(max_lag)]
+
+def effective_sample_size(samples):
+    n = len(samples)
+    ac_values = autocorrelation(samples, n)
+    tau = 1 + 2 * np.sum(ac_values)
+    return n / tau
+
+
+def plot_autocorrelation(samples, max_lag=100, save_path=None):
+    ac_values = autocorrelation(samples, max_lag)
+    plt.plot(range(max_lag), ac_values)
+    plt.xlabel('Lag')
+    plt.ylabel('Autocorrelation')
+    plt.title('Autocorrelation Plot')
+    plt.savefig(save_path)
+
+def e(samples, save_path=None):
+    plot_autocorrelation(samples, save_path=save_path)
+
+def f(samples):
+    ess = effective_sample_size(samples)
+    print(f'Effective Sample Size: {ess}')
+
+
+
+
+
 if __name__ == "__main__":
-    a()
-    b()
-    c()
+    theta_samples, lambda1_samples, lambda2_samples = a()
+
+    plt.clf()
+    # Using the results
+    e(theta_samples, save_path='p4_e_theta.png')
+    f(theta_samples)
+    plt.clf()
+
+    e(lambda1_samples, save_path='p4_e_lambda1.png')
+    f(lambda1_samples)
+    plt.clf()
+
+    e(lambda2_samples, save_path='p4_e_lambda2.png')
+    f(lambda2_samples)
+    
+    # b()
+    # c()
